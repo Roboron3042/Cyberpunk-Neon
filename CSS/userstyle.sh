59af630d47a265642a60711946f1eecbbc36cd7b
Roboron3042 <robertoms258@gmail.com>
2022-11-15 19:15:24 +0100

    UPD: Mastodon Cyberpunk Neon v4.0.0

--- CSS/userstyle.sh
+++ CSS/userstyle.sh
@@ -17,16 +17,24 @@ while getopts 's:v:' opt; do
 	esac
 done
 
-if [ "$STYLE" = "startpage" ]
-then
-echo "/* ==UserStyle==
-@name           Startpage Cyberpunk Neon
-@description    Cyberpunk Neon theme for Startpage" >> userstyle.css
+if [ "$STYLE" = "startpage" ]; then
+	NAME="Startpage"
+	URLS_PREFIX='url-prefix("https://www.startpage.com/")'
+	URL_GREASY="https://greasyfork.org/es/scripts/439540/versions/new"
+	URL_USER="https://userstyles.world/edit/3074"
+elif [ "$STYLE" = "mastodon" ]; then
+	NAME="Mastodon"
+	URLS_PREFIX='url-prefix("https://im-in.space/"), url-prefix("https://masto"), url-prefix("https://tkz.one")'
+	URL_GREASY="https://greasyfork.org/es/scripts/409167/versions/new"
+	URL_USER="https://userstyles.world/edit/2850"
 fi
 
-
-echo \
-"@namespace      github.com/Roboron3042/Cyberpunk-Neon
+echo "/* ==UserStyle==
+@name           $NAME Cyberpunk Neon
+@name:es        $NAME Cyberpunk Neon
+@description    Cyberpunk Neon theme for $NAME
+@description:es Tema Cyberpunk Neon para $NAME
+@namespace      github.com/Roboron3042/Cyberpunk-Neon
 @author         @Roboron3042 <robertoms258 at gmail dot com>
 @homepageURL    https://github.com/Roboron3042/Cyberpunk-Neon
 @supportURL     https://github.com/Roboron3042/Cyberpunk-Neon/issues
@@ -35,18 +43,12 @@ echo \
 @preprocessor   stylus
 ==/UserStyle== */" >> userstyle.css
 
-if [ "$STYLE" = "startpage" ]
-then
-	echo '@-moz-document url-prefix("https://www.startpage.com/") {' >> userstyle.css
-	cat startpage-cyberpunk-neon.css >> userstyle.css
-fi
+echo "@-moz-document $URLS_PREFIX {" >> userstyle.css
 
+cat $STYLE-cyberpunk-neon.css >> userstyle.css
 echo "}" >> userstyle.css
 
-if [ "$STYLE" = "startpage" ]
-then
-	xdg-open https://greasyfork.org/es/scripts/439540/versions/new
-	xdg-open https://userstyles.world/edit/3074
-fi
+xdg-open $URL_GREASY
+xdg-open $URL_USER
 
-rm userstyle.css
+#rm userstyle.css
